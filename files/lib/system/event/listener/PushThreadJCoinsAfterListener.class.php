1ff7df9273dee8a6e851bf8db055eb3f6deb7ea8
Sascha Greuel <github@1-2.dev>
2022-09-19 20:34:50 +0200

    5.5.1

--- files/lib/system/event/listener/PushThreadJCoinsAfterListener.class.php
+++ files/lib/system/event/listener/PushThreadJCoinsAfterListener.class.php
@@ -1,26 +1,51 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: https://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 namespace wbb\system\event\listener;
+
 use wcf\system\event\listener\IParameterizedEventListener;
 use wcf\system\user\jcoins\UserJCoinsStatementHandler;
 use wcf\system\WCF;
 
 /**
  * Listener for actions after push.
- *
- * @author		2020-2022 Zaydowicz
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.wbb.push.jcoins
  */
-class PushThreadJCoinsAfterListener implements IParameterizedEventListener {
-	/**
-	 * @inheritdoc
-	 */
-	public function execute($eventObj, $className, $eventName, array &$parameters) {
-		if (!MODULE_JCOINS || !MODULE_PUSH_THREAD) return;
-		
-		// assign JCoins unless canPushThreadUnlimited
-		if (WCF::getSession()->getPermission('user.board.canPushThreadUnlimited')) return;
-		
-		UserJCoinsStatementHandler::getInstance()->create('com.uz.wbb.push', $parameters['thread']);
-	}
+class PushThreadJCoinsAfterListener implements IParameterizedEventListener
+{
+    /**
+     * @inheritdoc
+     */
+    public function execute($eventObj, $className, $eventName, array &$parameters)
+    {
+        if (!MODULE_JCOINS || !MODULE_PUSH_THREAD) {
+            return;
+        }
+
+        // assign JCoins unless canPushThreadUnlimited
+        if (WCF::getSession()->getPermission('user.board.canPushThreadUnlimited')) {
+            return;
+        }
+
+        UserJCoinsStatementHandler::getInstance()->create('com.uz.wbb.push', $parameters['thread']);
+    }
 }
